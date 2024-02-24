214c7eb04ab454c8474c2b9e5f34e9a6702730b9
NuReLeS525 <122634975+NuReLeS525@users.noreply.github.com>
2024-02-24 20:54:31 +0600

    Delete dna directory

--- dna/main.js
+++
@@ -1,8 +0,0 @@
-let n = document.getElementsByClassName("pair").length;
-var root = document.documentElement;
-
-for (let i = 16; i>0; i--) {
-  console.log(i)
-  console.log(0.05*i.toString()+'s')
-  root.style.setProperty('--i', i.toString()+'s');
-}
