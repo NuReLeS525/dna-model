2ca6464419aa88d4c6970f7a9c54349f26eab4e2
NuReLeS525 <122634975+NuReLeS525@users.noreply.github.com>
2024-02-24 22:58:26 +0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,37 @@
-let n = document.getElementsByClassName("pair").length;
-var root = document.documentElement;
+const up = document.getElementById("up");
+const down = document.getElementById("down");
+let duration = 1.3;
+var dnaDuration = document.documentElement.style.setProperty('--dna-duration', duration.toString() + 's');
 
-for (let i = 16; i>0; i--) {
-  console.log(i)
-  console.log(0.05*i.toString()+'s')
-  root.style.setProperty('--i', i.toString()+'s');
+
+up.addEventListener("click", function(e) {
+  duration = duration - 0.1;
+  document.documentElement.style.setProperty('--dna-duration', duration.toString() + 's');
+  console.log(duration)
+});
+
+down.addEventListener("click", function(e) {
+  duration = duration + 0.1;
+  document.documentElement.style.setProperty('--dna-duration', duration.toString() + 's');
+  console.log(duration)
+});
+
+const pause = document.getElementById("pause");
+
+// pause.addEventListener("click", function(e) {
+//   document.documentElement.style.setProperty('--dna-duration', '0s')
+// });
+
+var toggled = false;
+function toggle() {
+  if (!toggled) {
+    toggled = true;
+    document.documentElement.style.setProperty('--dna-duration', '0s');
+    return;
+  }
+  if (toggled) {
+    toggled = false;
+    document.documentElement.style.setProperty('--dna-duration', duration.toString() + 's')
+    return;
+  }
 }
